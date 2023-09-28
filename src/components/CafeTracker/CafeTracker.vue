19e18fd94ea592a340cdc090231794c84f736f3d
Aaron Scofield <tuj83098@temple.edu>
2023-09-28 08:53:34 -0400

    load in google maps api

--- src/components/CafeTracker/CafeTracker.vue
+++ src/components/CafeTracker/CafeTracker.vue
@@ -1,13 +1,28 @@
 <template>
-  <p>CafeTracker</p>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div id="myMap"></div>
+  </div>
 </template>
 
 <script>
+const google=window.google
 export default {
-  name: 'CafeTracker'
+  name: 'hello',
+  mounted: function() {
+    this.map = new google.maps.Map(document.getElementById('myMap'), {
+    center: {lat:61.180059, lng: -149.822075},
+    scrollwheel: false,
+    zoom: 4
+  })
+}
+
 }
 </script>
-
-<style lang="scss" scoped>
-  @import './CafeTracker';
+<style scoped>
+    #myMap {
+    height:300px;
+    width: 100%;
+   }
 </style>
