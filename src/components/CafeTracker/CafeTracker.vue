e87de41f69302631e39d52d23b3d53d5203e8b1a
Aaron Scofield <aaron.scofield@icloud.com>
2023-10-24 21:02:53 -0400

    add home bio, update map placement/centering

--- src/components/CafeTracker/CafeTracker.vue
+++ src/components/CafeTracker/CafeTracker.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-
     <div id="myMap"></div>
   </div>
 </template>
@@ -12,9 +10,9 @@ export default {
   name: 'hello',
   mounted: function() {
     this.map = new google.maps.Map(document.getElementById('myMap'), {
-    center: {lat:61.180059, lng: -149.822075},
+    center: {lat:39.95233, lng: -75.16379},
     scrollwheel: false,
-    zoom: 4
+    zoom: 10
   })
 }
 
@@ -22,7 +20,7 @@ export default {
 </script>
 <style scoped>
     #myMap {
-    height:300px;
+    height:400px;
     width: 100%;
    }
 </style>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="home">
     <img src="../../static/IMG_4810.png">
+    <div class="bio">
+      <p>
+        Aaron Scofield is a Software Engineer from Philadelphia, PA. 
+        He graduated from Temple University in 2022 and has been designing
+        DevOps solutions at <a href="https://www.pgim.com/fixed-income/">PGIM Fixed Income</a> ever since. 
+        <br><br>
+        Aaron's current skillset includes and is certainly not limited to:
+      </p>
+        <div id="list">
+          <ul>
+            <li>AWS</li>
+            <li>Terraform</li>
+            <li>HashiCorp Vault</li>
+            <li>Jenkins</li>
+            <li>Self-Service Automation</li>
+            <li>Cloud and On-Prem Deployments</li>
+          </ul>
+          <ul>
+            <li>Python Development</li>
+            <li>API Development</li>
+            <li>Angular</li>
+            <li>React</li>
+            <li>Vue.js</li>
+          </ul>
+        </div>
+        <p>
+        In his free time, Aaron can be found walking around Philadelphia visiting various cafes (see the <a href="/cafe-tracker">custom cafe tracker</a>) &#9749;,
+        watching Phillies and Yankee games &#x26BE;, or attending the Philadelphia Orchestra's concerts &#x1F3BB;. 
+      </p>
+    </div>
   </div>
 </template>
 
