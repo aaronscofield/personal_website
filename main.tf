280e12465032fc713fa1ca9cdb5074867607ed76
Aaron Scofield <aaron.scofield@icloud.com>
2023-11-13 20:13:57 -0500

    create dynamodb table

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ module "public_s3_bucket" {
   source      = "./.terraform/modules/s3_bucket"
   bucket_name = "usea1-scofield-website-public-assets"
 }
+
+module "dynamo_db_table" {
+  source     = "./.terraform/modules/dynamodb_table"
+  table_name = "cafe-tracker-dynamodb-table"
+}
